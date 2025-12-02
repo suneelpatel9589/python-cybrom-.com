fd7de9d4edfccb7fb1b92e894042c3faba592098
suneel patel <suneelpatel9589@gmail.com>
2025-12-02 12:57:08 +0530

    module

---
+++ module.py
@@ -0,0 +1,37 @@
+# import module1
+# module1.mycollege()
+
+# import platform
+# x= dir(platform)
+# print(x)
+
+# y= platform.system()
+# print(y)
+
+# y= platform.release()
+# print(y)
+
+# y=platform.version()
+# print(y)
+
+# y=platform.system_alias('windows',10 ,'10.0.19045')
+# print(y)
+
+
+# import os
+# x= dir(os)
+# print(x)
+
+# y= os.getcwd
+# print(y)
+# os.mkdir('suneel')
+
+import datetime
+x= datetime.datetime.now() 
+print(x.month)
+print(x.day,"/",x.month,"/",x.year)
+
+y= datetime.datetime(2027,5,12,10,35,12) 
+print(y)
+
+
---
+++ module1.py
@@ -0,0 +1,4 @@
+def mycollege():
+    print('welcome to my collage')
+def myfunc(frm,lnm):
+    print(f"your name={frm} last name={lnm}")
